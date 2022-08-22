066594d8c4731f6c4075a758eb3582ac4ac64fca
abzahid <abdullahalzahidbd@gmail.com>
2022-08-22 17:56:29 +0600

    Js addeded

---
+++ js/panda.js
@@ -0,0 +1,80 @@
+// //Task 1 : CHhang the color of all h2 tag from the website
+// const allh2 = document.getElementsByTagName ('h2');
+// for(const h2 of allh2){
+//     console.log(h2);
+//     h2.style.color = 'lightblue'
+// }
+// //Task 2 : Change backgorund color of the backpack section to tomato
+// const backpackSection = document.getElementById('backpack');
+// console.log(backpackSection);
+// backpackSection.style.backgroundColor = 'tomato ';
+
+// //Task 3 : Put 30 px borderradius on all card class using js
+// const cards = document.getElementsByClassName('card');
+// console.log(cards);
+// for (const card of cards) {
+//  console.log(card);
+//  card.style.borderRadius = '30px';
+// }
+
+// //Task 4 : Use onclick eventhandler on a button
+
+// function clickOnButton(){
+//    console.log("nice button");
+// }
+
+// //Task 5 : Hide buy now button when you click on the buy now button
+
+// const allBuyNowButton = document.getElementsByClassName ('btn');
+// console.log(allBuyNowButton);
+// for (const button of allBuyNowButton) {
+//     console.log(button);
+//     button.addEventListener ('click',function(){
+//         button.style.display = 'none';
+
+//     })    
+
+
+//     //Task 6 : Make the submit button disabled in some special case and sometimes make it active
+
+//    const inputField =  document.getElementById('input-field');
+//    console.log(inputField);
+//    inputField.addEventListener('keyup',function(event){
+//    const inputText = event.target.value;
+//    console.log(inputText);
+//    if (inputText === "email") {
+//     const submitBtn = document.getElementById ('submit-btn');
+//     submitBtn.removeAttribute ("disabled");
+//    }
+//    else {
+//     submitBtn.setAttribute ("disabled",true);
+//    }
+//    })
+    
+// }
+
+
+
+//     function changePic (){
+//       const bag1 = document.getElementById ('one');
+//       bag1.src="images/bags/bag-2.png";
+
+
+
+
+//         }
+// function chagePic2 (){
+//     const bag2 = document.getElementById ('two');
+//     bag2.src="images/bags/bag-3.png";
+    
+// }
+// function staySame (){
+//     const bag1 = document.getElementById ('one');
+//     bag1.src="images/bags/bag-1.png";
+// }
+// function staySame2 (){
+//     const bag2 = document.getElementById ('two');
+//     bag2.src="images/bags/bag-2.png";
+    
+// }
+
